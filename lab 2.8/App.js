e003a866955e46c574cd319120bce473d699e4c5
chintd <119962869+chintd@users.noreply.github.com>
2023-04-11 18:10:01 +0700

    Add files via upload

---
+++ lab 2.8/App.js
@@ -0,0 +1,13 @@
+import Expenses from './Expenses/Expenses';
+import './App.css';
+import Card from './Expenses/Card';
+
+function App() {
+  return (
+    <Card className="App">
+      <Expenses/>
+    </Card>
+  );
+}
+
+export default App;
