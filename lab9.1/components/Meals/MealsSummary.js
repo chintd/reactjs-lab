3ab6ef280fa013f352d67e1fceb583b669b9d0ba
chintd <119962869+chintd@users.noreply.github.com>
2023-05-19 19:38:51 +0700

    Add files via upload

---
+++ lab9.1/components/Meals/MealsSummary.js
@@ -0,0 +1,10 @@
+import classes from "./MealsSummary.module.css";
+
+const MealsSummary=()=>{
+    return <div className={classes.summary}>
+        <h2>Delicious food, Delivery To You</h2>
+        <p> Choose your favorite meal from broad selection of availablt meals and enjoy a delicious lunch or dinner at home</p>
+        <p>All our meals are cooked with high-quality ingredients, just-in-time and of course by experienced chefs</p>
+    </div>
+}
+export default MealsSummary;
